b37eacd41886cbfdf8526b79b772da97b0218230
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-06 18:12:16 -0300

    ...........................................

--- Portfolio/marknow/Portfolio/Marketing agency/scripts/long-shadow.js
+++
@@ -1,5 +0,0 @@
-const boxElement = document.querySelectorAll(".block3d");
-
-for (let i = 0; i < 16; i++) {
-  boxElement[0].style.boxShadow += `-${i}px ${i}px black`;
-}
--- Portfolio/marknow/Portfolio/Marketing agency/scripts/rotating-txt.js
+++
@@ -1,9 +0,0 @@
-const text = document.querySelector(".txt");
-
-text.innerHTML = text.innerText
-  .split("")
-  .map(
-    (char, i) =>
-      `<span style="transform: rotate(${(i * 11.61290)}deg);">${char}</span>`
-  )
-  .join("");
